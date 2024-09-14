493d3281f6c4be9de4d60c881176566476c11d8c
markoscomks <markoscomks@gmail.com>
2024-09-14 20:01:08 +0100

    Solved issue with unitialized philo

--- philos_init.c
+++ philos_init.c
@@ -27,8 +27,9 @@ void	init_philos(t_table *table, t_philos *philos, char **av)
 	(void) table;
 
 	i = 0;
-	while (i < (size_t)ft_atoi(av[1]) - 1)
+	while (i < (size_t)ft_atoi(av[1]))
 	{
+		printf("Philo number %zi\n", i + 1);
 		philos[i].id = i + 1;
 		philos_input_data(&philos[i], av);
 		i++;
