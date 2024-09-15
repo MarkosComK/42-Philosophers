4846135a6313d9675f004730f12d163011f25828
markoscomks <markoscomks@gmail.com>
2024-09-15 16:16:32 +0100

    Added eating message

--- eat.c
+++ eat.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 10:32:22 by marsoare          #+#    #+#             */
-/*   Updated: 2024/09/15 13:32:10 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/09/15 16:16:04 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,13 @@ void	eat(t_philos *philo)
 	{
 		philo->forks++;
 		philo->table->forks--;
-		printf("start: %zi \n", philo->table->time);
-		printf("       %zi ", get_current_time() - philo->table->time);
+		printf("%zi ", get_current_time() - philo->table->time);
 		printf(BLUE"%i"GREEN" has taken a fork\n"DEFAULT, philo->id);
 	}
+	if (philo->forks == 2)
+	{
+		printf("%zi ", get_current_time() - philo->table->time);
+		printf(BLUE"%i"GREEN" is eating\n"DEFAULT, philo->id);
+	}
 	pthread_mutex_unlock(&philo->table->mutex);
-	printf("\n");
 }
