4b62cd05b35ec9829f6d7534c213416790221bc8
markoscomks <markoscomks@gmail.com>
2024-09-15 09:51:10 +0100

    Norm ok

--- utils.c
+++ utils.c
@@ -23,6 +23,7 @@ size_t	ft_strlen(char *str)
 		size++;
 	return (size);
 }
+
 int	ft_atoi(char *str)
 {
 	int	result;
@@ -70,11 +71,11 @@ void	print_philos(t_philos *philo)
 	while (i < philo[0].num_philos)
 	{
 		printf("philo: %zi\n", i + 1);
-		printf(YELLOW"philo->num_philos:"GREEN" [% 18zi]\n", philo[i].num_philos);
-		printf(YELLOW"philo->forks:"GREEN"      [% 18d]\n", philo[i].forks);
-		printf(YELLOW"philo->time-die:"GREEN"   [% 18zi]\n", philo[i].time_die);
-		printf(YELLOW"philo->time-eat:"GREEN"   [% 18zi]\n", philo[i].time_eat);
-		printf(YELLOW"philo->time-sleep:"GREEN" [% 18zi]\n", philo[i].time_sleep);
+		printf(YELLOW"philo->num_philos:"GREEN"[% 8zi]\n", philo[i].num_philos);
+		printf(YELLOW"philo->forks:"GREEN"     [% 8d]\n", philo[i].forks);
+		printf(YELLOW"philo->time-die:"GREEN"  [% 8zi]\n", philo[i].time_die);
+		printf(YELLOW"philo->time-eat:"GREEN"  [% 8zi]\n", philo[i].time_eat);
+		printf(YELLOW"philo->time-sleep:"GREEN"[% 8zi]\n", philo[i].time_sleep);
 		printf("\n"DEFAULT);
 		i++;
 	}
