61f78e992cd8949c68f894a0fa71cc0ce8ad4d81
markoscomks <markoscomks@gmail.com>
2024-09-15 09:49:02 +0100

    Norm ok

--- handle_meal.c
+++ handle_meal.c
@@ -14,45 +14,34 @@
 
 void	*routine(void *arg)
 {
-	t_philos *philo;
+	t_philos	*philo;
 
 	philo = (t_philos *)arg;
 	if (philo->id % 2 == 0)
 		ft_usleep(1);
-	//pthread_mutex_lock(&((t_table *)arg)->mutex);
 	printf("Hi im philo N: %i\n", philo->id);
-	//pthread_mutex_unlock(&((t_table *)arg)->mutex);
-	//somehow implement mutexes for forks
-	//eat
-	//sleep
-	//think
 	return (arg);
 }
 
 void	*observe(void *arg)
 {
-	//pthread_mutex_lock(&((t_table *)arg)->mutex);
 	printf(BLUE"Hi im the waiter!\n"DEFAULT);
-	//pthread_mutex_unlock(&((t_table *)arg)->mutex);
-	//somehow implement mutexes for forks
-	//eat
-	//sleep
-	//think
 	return (arg);
 }
 
 void	start_meal(t_table *table, t_philos *philos, char **av)
 {
 	size_t	i;
-	(void) av, (void) philos;
 
+	(void) av;
+	(void) philos;
 	i = 0;
 	if (pthread_create(&table->table, NULL, observe, NULL) != 0)
-			write(2, "TError\n", 7);
+		write(2, "TError\n", 7);
 	while (i < table->philos->num_philos)
 	{
-		if(pthread_create(&table->philos[i].thread, NULL,
-			routine, &table->philos[i]) != 0)
+		if (pthread_create(&table->philos[i].thread, NULL,
+				routine, &table->philos[i]) != 0)
 			write(2, "TError\n", 7);
 		i++;
 	}
@@ -66,8 +55,8 @@ void	start_meal(t_table *table, t_philos *philos, char **av)
 
 void	end_meal(t_table *table, t_philos *philos, char **av)
 {
-	(void) av, (void) philos;
-
+	(void) av;
+	(void) philos;
 	pthread_join(table->table, NULL);
 	pthread_mutex_destroy(&table->mutex);
 }
