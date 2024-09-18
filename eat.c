f10504876152f68f99a1f7d057464cae7c88b0ca
markoscomks <markoscomks@gmail.com>
2024-09-18 19:03:15 +0100

    Removed warning generated by fsanitize

--- eat.c
+++ eat.c
@@ -14,19 +14,21 @@
 
 void	eat(t_philos *philo)
 {
-	t_philos *next;
-	int	last;
+	t_philos	*next;
+	t_philos	*own;
 
-	last = philo->num_philos;
+	own = philo;
 
-	if (philo->id == last)
-		next = &philo->table->philos[0];
+	if (philo->id == (int)philo->num_philos)
+	{
+		next = &philo->table->philos[philo->id];
+		own = &philo->table->philos[0];
+	}
 	else
 		next = &philo->table->philos[philo->id];
 	//printf("my fork %p \nnext %p\n\n", &philo->fork, &next->fork);
 //lock own fork
-	if(pthread_mutex_lock(&philo->fork))
-		printf("error taking own fork");
+	pthread_mutex_lock(&own->fork);
 	printf("%zi ", get_current_time() - philo->table->time);
 	printf(BLUE"%i"GREEN" has taken a fork\n"DEFAULT, philo->id);
 //lock next fork
@@ -41,7 +43,7 @@ void	eat(t_philos *philo)
 //after eat goes sleep
 	printf("%zi ", get_current_time() - philo->table->time);
 	printf(BLUE"%i"GREEN" is sleeping\n"DEFAULT, philo->id);
-	if (pthread_mutex_unlock(&philo->fork))
+	if (pthread_mutex_unlock(&own->fork))
 		printf("error unlocking own fork\n");
 	if (pthread_mutex_unlock(&next->fork))
 		printf("error unlocking next fork\n");
