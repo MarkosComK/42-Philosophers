db801f56d1b4c57e237c20205536a5a412a26256
markoscomks <markoscomks@gmail.com>
2024-09-21 11:30:02 +0100

    Norm ok

--- philo.h
+++ philo.h
@@ -24,18 +24,18 @@
 
 typedef struct s_philos
 {
-	int			id;
-	pthread_t	thread;
-	size_t		num_philos;
+	int				id;
+	pthread_t		thread;
+	size_t			num_philos;
 	pthread_mutex_t	*fork;
 	pthread_mutex_t	*mealtex;
-	size_t		time_die;
-	size_t		time_eat;
-	size_t		time_sleep;
-	size_t		last_meal;
-	int			sleep;
-	int			eat;
-	int			*dead;
+	size_t			time_die;
+	size_t			time_eat;
+	size_t			time_sleep;
+	size_t			last_meal;
+	int				sleep;
+	int				eat;
+	int				*dead;
 	struct s_table	*table;
 }				t_philos;
 
@@ -46,7 +46,7 @@ typedef struct s_table
 	pthread_mutex_t	mutex;
 	pthread_mutex_t	*deadtex;
 	unsigned long	time;
-	int			dead_flag;
+	int				dead_flag;
 }				t_table;
 
 void	prepare_table(t_philos *philos, t_table *table, char **av);
@@ -61,16 +61,16 @@ void	print_philos(t_philos *philo);
 void	destroy_all(char *str, t_table *table, pthread_mutex_t *forks);
 int		ft_usleep(size_t milliseconds);
 size_t	get_current_time(void);
-int	ft_strcmp(char *s1, char *s2);
+int		ft_strcmp(char *s1, char *s2);
 //actions
 void	eat(t_philos *philos);
 void	rivotril(t_philos *philo);
 void	sophos(t_philos *philo);
 int		thread_printf(t_philos *philo, char	*msg);
 //waiter
-int	philosophers_state(t_philos *philo);
-int	philosopher_dead(t_philos *philo);
-int	philo_dead(t_philos *philo);
+int		philosophers_state(t_philos *philo);
+int		philosopher_dead(t_philos *philo);
+int		philo_dead(t_philos *philo);
 void	*waiter(void *arg);
 
 #endif
