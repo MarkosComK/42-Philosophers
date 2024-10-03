4ff4813e840e219a5c9946e0a5369b8184f91432
markoscomks <markoscomks@gmail.com>
2024-10-03 15:21:59 +0100

    Changed fork grab order and added mutex to mark eat

--- philo/eat.c
+++ philo/eat.c
@@ -14,26 +14,29 @@
 
 void	eat(t_philos *philo)
 {
-	t_philos	next;
-	t_philos	own;
+	pthread_mutex_t	*next_fork;
+	pthread_mutex_t	*own_fork;
 
-	own = *philo;
-	if (philo->id == (int)philo->num_philos)
-		next = philo->table->philos[0];
-	else
-		next = philo->table->philos[philo->id];
-	pthread_mutex_lock(own.fork);
+	own_fork = &philo->table->forks[(philo->id - 1) % philo->num_philos];
+	next_fork = &philo->table->forks[(philo->id) % philo->num_philos];
+	if (philo->id % 2 == 0) {
+		next_fork = own_fork;
+		own_fork = &philo->table->forks[(philo->id) % philo->num_philos];
+	}
+	pthread_mutex_lock(own_fork);
 	thread_printf(philo, "has taken a fork");
-	pthread_mutex_lock(next.fork);
+	pthread_mutex_lock(next_fork);
 	thread_printf(philo, "has taken a fork");
 	thread_printf(philo, "is eating");
-	pthread_mutex_lock(own.mealtex);
+	pthread_mutex_lock(philo->mealtex);
 	philo->last_meal = get_current_time();
+	pthread_mutex_lock(philo->table->eatentex);
 	philo->eaten++;
-	pthread_mutex_unlock(own.mealtex);
+	pthread_mutex_unlock(philo->table->eatentex);
+	pthread_mutex_unlock(philo->mealtex);
 	ft_usleep(philo->time_eat);
-	pthread_mutex_unlock(own.fork);
-	pthread_mutex_unlock(next.fork);
+	pthread_mutex_unlock(own_fork);
+	pthread_mutex_unlock(next_fork);
 }
 
 void	rivotril(t_philos *philo)
