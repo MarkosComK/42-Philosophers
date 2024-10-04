2afd7f01bbd0732302d7c5751001ec2b8173e630
markoscomks <markoscomks@gmail.com>
2024-10-04 15:46:27 +0100

    Removed debugger printf

--- philo_bonus/routine.c
+++ philo_bonus/routine.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 15:42:02 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/04 15:42:14 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/04 15:45:52 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	*routine(void *arg)
 {
 	t_philos	*philo = (t_philos *)arg;
 	printf("Hi, i'm thread with id: %i\n", philo->id);
-	print_philo(philo);
+	//print_philo(philo);
 	return (arg);
 }
