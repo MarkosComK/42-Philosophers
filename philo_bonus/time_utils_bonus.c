51f0a92888fd661f7b6c84afa091486f61b85cfc
markoscomks <markoscomks@gmail.com>
2024-10-12 15:36:48 +0100

    added usleep

--- philo_bonus/time_utils_bonus.c
+++ philo_bonus/time_utils_bonus.c
@@ -46,3 +46,13 @@ void	ft_wait(t_philos *philos, size_t interval)
 		usleep(100);
 	}
 }
+
+int	ft_usleep(size_t milliseconds)
+{
+	size_t	start;
+
+	start = get_current_time();
+	while ((get_current_time() - start) < milliseconds)
+		usleep(100);
+	return (0);
+}
